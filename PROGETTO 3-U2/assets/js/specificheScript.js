507c41f0b5b2afd5b748e3b7448c03e380e58058
Simonecaste96 <simonecaste96@gmail.com>
2024-03-10 15:18:59 +0100

    aggiunta pagina specifiche dettagli...

--- PROGETTO 3-U2/assets/js/specificheScript.js
+++ PROGETTO 3-U2/assets/js/specificheScript.js
@@ -1,15 +1,84 @@
 const URL = 'https://striveschool-api.herokuapp.com/api/product/';
 const API_KEY = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhY2M2YTJkN2IxMTAwMTkwZTZkYzIiLCJpYXQiOjE3MDk4ODY1NzAsImV4cCI6MTcxMTA5NjE3MH0.kEOi2NranqbfjVFDEjR6u_0lDBLsg7h9G3KKljeZjoY';
 
+const divSpecifiche = document.getElementById('specifiche');
 
-// div class="card mb-3">
-//   <img src="..." class="card-img-top" alt="...">
-//   <div class="card-body">
-//     <h5 class="card-title">Card title</h5>
-//     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-//     <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
-//   </div>
-// </div>
+
+
+//Stesso discorso del js del backOffice, mi riprendo i dati con una get, avevo valutato l'opzione local storage ma in ottica rale con una quantità di dati sconosciuta non l'ho ritenuto un metodolo 'prestante' difatti è proprio un copia e incolla, easy.
+
+
+
+//Variabili per identificare se c'è la query!!!!
+let query = window.location.search;
+
+// Estraggo l'ID dalla query (se prsente) URL
+const urlParametro = new URLSearchParams(query);
+const urlIdParametro = urlParametro.get('id');
+
+window.addEventListener('load', init);
+
+function init(){
+ dettagli();   
+}
+
+
+function dettagli(){
+    //Se nel'url c'è la query, l'h1 diventa modifica prodotto, altrimenti rimane invariato. 
+    if(query){
+   
+
+    // Effettuo la richiesta al server per ottenere i dettagli del prodotto che voglio modificare!
+    fetch(`${URL}/${urlIdParametro}`, {
+      headers: {
+        "Content-Type": "application/json",
+          Authorization: API_KEY,
+      },
+    })
+    .then(response => {
+      if (response.ok) {
+      return response.json();    
+      }
+      
+    })
+    
+    // Popolo il form con i dettagli del prodotto ottenuti dalla risposta del server....
+    .then(product => {
+      const  nameModel = product.name;
+      const model = product.brand;
+      const price = product.price;
+      const url = product.imageUrl;
+      const description = product.description;
+      
+    divSpecifiche.innerHTML = `<div class="card mt-4 d-flex flex-row">
+    <div><img src="${url}" class="card-img-top w-100" alt="${nameModel}"></div>
+  <div class="card-body">
+    <p class="card-text">${model}</p>
+    <h4 class="card-title">${nameModel}</h4>
+    <p class="card-text">Prezzo: ${price}€</p>
+    <p class="card-text">${description}</p>
+  </div>
+</div>`;
+
+     
+    });
+    
+    }
+    
+    };
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
